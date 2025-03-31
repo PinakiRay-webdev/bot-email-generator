d957954b42c869fd666639d60f18045af02f805f
PinakiRay-webdev <pinaki.mongodb@gmail.com>
2025-03-31 17:55:24 +0530

    dynamic rendering of account section of navbar

--- src/components/main/components/Navbar/Navbar.jsx
+++ src/components/main/components/Navbar/Navbar.jsx
@@ -1,39 +1,83 @@
-import {useState , useEffect} from 'react'
-import { useNavigate } from 'react-router-dom';
-const Navbar = () => {
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { IoIosLogOut } from "react-icons/io";
+import { signOut } from "firebase/auth";
+import { auth } from "../../../../firebase/firebaseConfig";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
+const Navbar = () => {
   const [greet, setGreet] = useState("");
 
-  const displayGreet = () =>{
+  const userData = JSON.parse(localStorage.getItem("userCredentials"));
+
+  const displayGreet = () => {
     const date = new Date();
     const hours = date.getHours();
-    if(hours >= 5 && hours < 12){
+    if (hours >= 5 && hours < 12) {
       setGreet("Good morning");
-    }else if(hours >= 12 && hours < 18){
-      setGreet("Good afternoon")
-    }else if(hours >= 18 && hours < 23){
-      setGreet("Good evening")
-    }else {
-      setGreet("Still awake!")
+    } else if (hours >= 12 && hours < 18) {
+      setGreet("Good afternoon");
+    } else if (hours >= 18 && hours < 23) {
+      setGreet("Good evening");
+    } else {
+      setGreet("Still awake!");
     }
-  }
+  };
 
   const navigate = useNavigate();
 
-  useEffect(() =>{
-    displayGreet()
-  } , []) 
+  const Logout = async () => {
+    toast.loading("signing you out...", { theme: "dark" });
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 1200);
+    }).then(() => {
+      toast.dismiss();
+      signOut(auth)
+        .then(() => {
+          localStorage.clear();
+          toast.success("logged out successfully", { theme: "dark" });
+        })
+        .catch((error) => {
+          toast.error(`Error: ${error.message}`, { theme: "dark" });
+        });
+    });
+  };
 
+  useEffect(() => {
+    displayGreet();
+  }, []);
 
   return (
-    <div className='text-white flex justify-between items-center' >
-      <h1 className='text-3xl' >{greet}</h1>
-      <section id='accounts' className='flex gap-2' >
-        <button className='ring-1 ring-gray-500 px-4 py-1 rounded' >Get started</button>
-        <button onClick={() => navigate('/login')} className='ring-1 bg-gray-500 px-4 py-1 rounded' >Login</button>
-      </section>
-    </div>
-  )
-}
+    <div>
+      <div className="text-white flex justify-between items-center">
+        <h1 className="text-3xl">{greet}</h1>
 
-export default Navbar
+        {!userData ? (
+          <section id="accounts" className="flex gap-2">
+            <button className="ring-1 ring-gray-500 px-4 py-1 rounded">
+              Get started
+            </button>
+            <button
+              onClick={() => navigate("/login")}
+              className="ring-1 bg-gray-500 px-4 py-1 rounded"
+            >
+              Login
+            </button>
+          </section>
+        ) : (
+          <section>
+            <p onClick={Logout} className="text-4xl cursor-pointer">
+              <IoIosLogOut />
+            </p>
+          </section>
+        )}
+      </div>
+        <ToastContainer />
+    </div>
+  );
+};
+
+export default Navbar;
